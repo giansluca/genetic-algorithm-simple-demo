b960220f61a0bd5f414ce7a302814190acdfd78a
gians <giansluca@gmail.com>
2024-07-04 12:38:19 +0200

    first commit

---
+++ src/main/java/org/gmdev/Main.java
@@ -0,0 +1,30 @@
+package org.gmdev;
+
+import org.gmdev.genetic.Algorithm;
+import org.gmdev.genetic.FitnessCalc;
+import org.gmdev.genetic.Population;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Set a candidate solution
+        FitnessCalc.setSolution("1111000000000000000000000000000000000000000000000000000000001111");
+
+        // Create an initial population
+        Population myPop = new Population(50, true);
+
+        // Evolve our population until we reach an optimum solution
+        int generationCount = 0;
+        while (myPop.getFittest().getFitness() < FitnessCalc.getMaxFitness()) {
+            generationCount++;
+            System.out.println("Generation: " + generationCount + " Fittest: " + myPop.getFittest().getFitness() + " Max Fitness: " + FitnessCalc.getMaxFitness());
+            myPop = Algorithm.evolvePopulation(myPop);
+        }
+
+        System.out.println("Solution found!");
+        System.out.println("Generation: " + generationCount);
+        System.out.println("Genes:");
+        System.out.println(myPop.getFittest());
+    }
+
+}
